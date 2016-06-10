f5d9dc4c5bbe4ff87a89c545614b7e1e2a27d82d
JeremyMcCormick <jermccormick@gmail.com>
2016-06-09 19:39:43 -0700

    add plugin config for JAS

---
+++ plugin/src/main/java/org/ldmx/plugin/LDMXPlugin.java
@@ -0,0 +1,11 @@
+package org.ldmx.plugin;
+
+import org.freehep.application.studio.Plugin;
+
+/**
+ * This is just a dummy plugin class to make JAS3 happy.
+ * @author Jeremy McCormick
+ */
+
+public class LDMXPlugin extends Plugin {
+}
